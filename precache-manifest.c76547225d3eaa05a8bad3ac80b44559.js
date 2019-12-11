8de0ddc6234e9153390140ed2cd0f977d8ecf13c
the-jack-white <46023638+the-jack-white@users.noreply.github.com>
2019-12-11 10:56:24 +0200

    Updates

---
+++ precache-manifest.c76547225d3eaa05a8bad3ac80b44559.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cccd4af1213364480a291b966a51d6a6",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "a97c08fb6ee0e3959249",
+    "url": "/myportfolio/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "e8e386facf9cb07f1a51",
+    "url": "/myportfolio/static/css/main.cc424eca.chunk.css"
+  },
+  {
+    "revision": "a97c08fb6ee0e3959249",
+    "url": "/myportfolio/static/js/2.8f27551c.chunk.js"
+  },
+  {
+    "revision": "1a3ba1185bf697297fa4b180bfc93f40",
+    "url": "/myportfolio/static/js/2.8f27551c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e8e386facf9cb07f1a51",
+    "url": "/myportfolio/static/js/main.021c10a5.chunk.js"
+  },
+  {
+    "revision": "cd2dc230974440385eb0",
+    "url": "/myportfolio/static/js/runtime-main.a353a1d1.js"
+  }
+]);
