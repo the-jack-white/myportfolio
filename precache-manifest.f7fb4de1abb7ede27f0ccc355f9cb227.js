e1b96a9ad3c9faacb8e56112c0e5104cac4e7db2
the-jack-white <46023638+the-jack-white@users.noreply.github.com>
2019-12-11 11:08:49 +0200

    Updates

---
+++ precache-manifest.f7fb4de1abb7ede27f0ccc355f9cb227.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1ce74b68fb3561f22d9a5523c4d77b4",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "a97c08fb6ee0e3959249",
+    "url": "/myportfolio/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "1e3a58317e2a85389ba0",
+    "url": "/myportfolio/static/css/main.cc424eca.chunk.css"
+  },
+  {
+    "revision": "a97c08fb6ee0e3959249",
+    "url": "/myportfolio/static/js/2.8f27551c.chunk.js"
+  },
+  {
+    "revision": "1a3ba1185bf697297fa4b180bfc93f40",
+    "url": "/myportfolio/static/js/2.8f27551c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e3a58317e2a85389ba0",
+    "url": "/myportfolio/static/js/main.77c65806.chunk.js"
+  },
+  {
+    "revision": "cd2dc230974440385eb0",
+    "url": "/myportfolio/static/js/runtime-main.a353a1d1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f6c89579c197bfd4ab3e28ba2783e139.js"
+  "/myportfolio/precache-manifest.f7fb4de1abb7ede27f0ccc355f9cb227.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myportfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
