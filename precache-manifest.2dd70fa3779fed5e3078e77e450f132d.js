cd78cbda28e61261b0cf20d2f4d7d6c82752368b
the-jack-white <46023638+the-jack-white@users.noreply.github.com>
2019-12-11 12:16:39 +0200

    Updates

--- precache-manifest.2dd70fa3779fed5e3078e77e450f132d.js
+++ precache-manifest.2dd70fa3779fed5e3078e77e450f132d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "77a71728dbc47bc2c271bd2b02ad0f88",
+    "revision": "f9351b19ad35cf38a862b0a7843ebf4b",
     "url": "/myportfolio/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/myportfolio/static/css/2.64a99b10.chunk.css"
   },
   {
-    "revision": "55f0344fc5507ab00529",
-    "url": "/myportfolio/static/css/main.f5837a04.chunk.css"
+    "revision": "5a49196a435370f1760b",
+    "url": "/myportfolio/static/css/main.1995c55c.chunk.css"
   },
   {
     "revision": "a97c08fb6ee0e3959249",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/myportfolio/static/js/2.8f27551c.chunk.js.LICENSE"
   },
   {
-    "revision": "55f0344fc5507ab00529",
+    "revision": "5a49196a435370f1760b",
     "url": "/myportfolio/static/js/main.1dd9af47.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myportfolio/precache-manifest.16480c277d6b6330bf2e715eb22631d1.js"
+  "/myportfolio/precache-manifest.2dd70fa3779fed5e3078e77e450f132d.js"
 );
 
 self.addEventListener('message', (event) => {
